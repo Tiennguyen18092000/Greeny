132e0f33ff12431ddef0b7eff746007674e46634
duc <hohuynhduca7bh123@gmail.com>
2020-08-26 15:13:11 +0700

    add firebase and nebular

--- src/environments/dist/environment.js
+++ src/environments/dist/environment.js
@@ -1,21 +1,22 @@
+"use strict";
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyCknh08F80zRAX0-4RJUXDuzA73QEMxpjM",
-    authDomain: "greeny-8e32c.firebaseapp.com",
-    databaseURL: "https://greeny-8e32c.firebaseio.com",
-    projectId: "greeny-8e32c",
-    storageBucket: "greeny-8e32c.appspot.com",
-    messagingSenderId: "379187712053",
-    appId: "1:379187712053:web:ce411af6e0b9fad16d617c",
-    measurementId: "G-GG8V4KY55H"
-  }
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCknh08F80zRAX0-4RJUXDuzA73QEMxpjM",
+        authDomain: "greeny-8e32c.firebaseapp.com",
+        databaseURL: "https://greeny-8e32c.firebaseio.com",
+        projectId: "greeny-8e32c",
+        storageBucket: "greeny-8e32c.appspot.com",
+        messagingSenderId: "379187712053",
+        appId: "1:379187712053:web:ce411af6e0b9fad16d617c",
+        measurementId: "G-GG8V4KY55H"
+    }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
---
+++ src/environments/dist/environment.prod.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCknh08F80zRAX0-4RJUXDuzA73QEMxpjM",
+        authDomain: "greeny-8e32c.firebaseapp.com",
+        databaseURL: "https://greeny-8e32c.firebaseio.com",
+        projectId: "greeny-8e32c",
+        storageBucket: "greeny-8e32c.appspot.com",
+        messagingSenderId: "379187712053",
+        appId: "1:379187712053:web:ce411af6e0b9fad16d617c",
+        measurementId: "G-GG8V4KY55H"
+    }
+};
